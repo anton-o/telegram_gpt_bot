24ddd8e453198db80a7fb423c4ae09396cc29d4e
anton-o <anton.ovchinnikov@gmail.com>
2023-08-14 00:18:03 +0300

    secrets example added

---
+++ bot_secrets EXAMPLE.py
@@ -0,0 +1,14 @@
+# telegram token
+BOT_TOKEN = "your_token_from_@botfather"
+
+# openai token
+OPENAI_KEY = 'your_openai_api_key'
+
+# telegram user IDs for operations like whoami and direct communication with the bot
+_admins = [0000000, ]
+
+# telegram group IDs which members will be able to ask bot with @botname
+_groups = [-1111111, ]
+
+#telegram botname, to be used for explicit bot referecning with @
+BOT_NAME = 'yourbotname_bot'
